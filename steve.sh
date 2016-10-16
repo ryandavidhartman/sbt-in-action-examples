de1edd83434da8fead84d76b824363f950b019b6
ryandavidhartman <ryandavidhartman@gmail.com>
2016-10-15 23:07:28 -0400

    whoops, a few mistakes from the last refactor

--- steve.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p chapter03/common/src/main/scala/org/pragmaticdemo
-mkdir -p chapter04/common/src/main/scala/org/pragmaticdemo
-mkdir -p chapter05/common/src/main/scala/org/pragmaticdemo
-mkdir -p chapter06/common/src/main/scala/org/pragmaticdemo
-mkdir -p chapter07/common/src/main/scala/org/pragmaticdemo
-mkdir -p chapter08/common/src/main/scala/org/pragmaticdemo
-mkdir -p chapter10/common/src/main/scala/org/pragmaticdemo
-mkdir -p chapter12/common/src/main/scala/org/pragmaticdemo
